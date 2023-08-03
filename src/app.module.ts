b77475639b0b3c19b5879b8bf0c1aec41b12a53a
lucdticd-vn <lucdt@icd-vn.com>
2023-08-03 15:13:42 +0700

    update pk

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,14 @@ import { THROTTLE_LIMIT, THROTTLE_TTL } from '@utils/constants';
 import { DatabaseModule } from '@database/database.module';
 import { ImageModule } from '@modules/image/image.module';
 import { ImageCategoryModule } from '@modules/image-category/image-category.module';
+import { AuthModule } from '@modules/auth/auth.module';
 
 @Module({
   imports: [
     DatabaseModule,
     ImageModule,
     ImageCategoryModule,
+    AuthModule,
     ThrottlerModule.forRoot({
       ttl: THROTTLE_TTL,
       limit: THROTTLE_LIMIT,
